b9758175267a2cae19f68c5cc441ffae7827d529
magro-ga <gabas9630@gmail.com>
2020-06-28 00:52:38 -0300

    aprofundando no storage

--- Ex_JS_em_HTML/todo.js
+++ Ex_JS_em_HTML/todo.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var btnElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar JavaScript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos =  JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -41,6 +37,7 @@ function addTodo() {
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 btnElement.onclick = addTodo;
@@ -48,4 +45,9 @@ btnElement.onclick = addTodo;
 function deleteTodo(pos){
     todos.splice(pos, 1);
     renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
